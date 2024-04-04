69f9a291d5fe28183abb2723f84f1516bb821c03
Omar Terrazas <terrazas.omar@gmail.com>
2024-04-04 11:44:47 -0600

    Integración del menú superior

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,12 +1,12 @@
-import { useRoutes, BrowserRouter } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import Home from '../Home/Home';
-import NotFound from '../NotFound/NotFound';
-import './App.css';
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import Home from "../Home/Home";
+import NotFound from "../NotFound/NotFound";
+import "./App.css";
 
 const AppRoutes = () => {
   return useRoutes([
-    { path: '/', element: <Home /> },
+    { path: "/", element: <Home /> },
     { path: "*", element: <NotFound /> },
   ]);
 };
@@ -14,9 +14,28 @@ const AppRoutes = () => {
 function App() {
   return (
     <BrowserRouter>
-      {/*<nav>
-        <NavLink to="/">Home</NavLink>
-  </nav>*/}
+      <nav>
+        <a href="https://aahd.net.ar/">
+          <img
+            src="images/aahd.png"
+            alt="Asociación Argentina de Humanidades Digitales"
+            width={60}
+          />
+        </a>
+        <a href="https://rchd.com.co/">
+          <img src="images/rchd.png" alt="Red Colombiana de Humanidades Digitales" width={78} />
+        </a>
+        <a href="http://humanidadesdigitales.net/">
+          <img src="images/rhd.png" alt="Red de Humanidades Digitales" width={68} />
+        </a>
+        <a href="https://cuaieed.unam.mx/"><img src="images/cuaieed.svg" alt="Coordinación de Universidad Abierta y Educación Digital" width={60} /></a>
+        <NavLink to="/proposal" className="proposal">Propón un evento</NavLink>
+        <div className="colorbar">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </nav>
       <AppRoutes />
     </BrowserRouter>
   );
