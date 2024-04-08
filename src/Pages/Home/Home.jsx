b1317e4bd76c0f883a686c0ac5736b0643c2bc31
Omar Terrazas <terrazas.omar@gmail.com>
2024-04-08 16:47:03 -0600

    Se integran secciones faltantes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,9 @@ import Hero from "../../Components/Home/Hero/Hero";
 import About from "../../Components/Home/About/About";
 import Invite from "../../Components/Home/Invite/Invite";
 import Calendar from "../../Components/Home/Calendar/Calendar";
+import Identity from "../../Components/Home/Identity/Identity";
+import Previous from "../../Components/Home/Previous/Previous";
+import LatamHD from "../../Components/Home/LatamHD/LatamHD";
 
 function Home() {
   return (
@@ -10,6 +13,9 @@ function Home() {
       <About />
       <Invite />
       <Calendar />
+      <Identity />
+      <Previous />
+      <LatamHD />
     </>
   );
 }
