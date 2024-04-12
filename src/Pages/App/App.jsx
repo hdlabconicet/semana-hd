195d519ee3fde9e9336df2cd6c74ad7a3065b743
Omar Terrazas <terrazas.omar@gmail.com>
2024-04-11 19:31:41 -0600

    Se ajustan logos en header

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -10,8 +10,8 @@ import "./App.css";
 const AppRoutes = () => {
   return useRoutes([
     { path: "/", element: <Home /> },
-    { path: "/proposal", element: <Proposal />},
-    { path: "/calendar", element: <Calendar />},
+    { path: "/proposal", element: <Proposal /> },
+    { path: "/calendar", element: <Calendar /> },
     { path: "*", element: <NotFound /> },
   ]);
 };
@@ -20,21 +20,60 @@ function App() {
   return (
     <BrowserRouter>
       <nav>
-        <a href="https://aahd.net.ar/" target="_blank" rel="noopener noreferrer">
+        <NavLink to="/">
+          <img
+            src="images/semanahd-logo.jpg"
+            alt="Semana de Humanidades Digitales"
+            width={158}
+          />
+        </NavLink>
+        <a
+          href="https://cuaieed.unam.mx/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="images/cuaed.png"
+            alt="Coordinación de Universidad Abierta y Educación Digital"
+            width={166}
+          />
+        </a>
+        <a
+          href="https://aahd.net.ar/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
             src="images/aahd.png"
             alt="Asociación Argentina de Humanidades Digitales"
             width={60}
           />
         </a>
-        <a href="https://rchd.com.co/" target="_blank" rel="noopener noreferrer">
-          <img src="images/rchd.png" alt="Red Colombiana de Humanidades Digitales" width={78} />
+        <a
+          href="https://rchd.com.co/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="images/rchd.png"
+            alt="Red Colombiana de Humanidades Digitales"
+            width={78}
+          />
         </a>
-        <a href="http://humanidadesdigitales.net/" target="_blank" rel="noopener noreferrer">
-          <img src="images/rhd.png" alt="Red de Humanidades Digitales" width={68} />
+        <a
+          href="http://humanidadesdigitales.net/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="images/rhd.png"
+            alt="Red de Humanidades Digitales"
+            width={68}
+          />
         </a>
-        <a href="https://cuaieed.unam.mx/" target="_blank" rel="noopener noreferrer"><img src="images/cuaieed.svg" alt="Coordinación de Universidad Abierta y Educación Digital" width={60} /></a>
-        <NavLink to="/proposal" className="proposal">Propón un evento</NavLink>
+        <NavLink to="/proposal" className="proposal">
+          Propón un evento
+        </NavLink>
         <ColorBar />
       </nav>
       <AppRoutes />
@@ -42,13 +81,45 @@ function App() {
         <div className="container mx-auto p-2 text-white text-sm flex flex-col lg:flex-row">
           <div className="lg:w-1/6 mb-6">
             <p className="mt-7 mb-4">Desarrollado por:</p>
-            <p><img src="images/bunam_mooc.svg" alt="B@UNAM & MOOC" width={100} /></p>
+            <p>
+              <a
+                href="https://bunam.unam.mx/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="images/bunam_mooc.svg"
+                  alt="B@UNAM & MOOC"
+                  width={100}
+                />
+              </a>
+            </p>
           </div>
           <div className="lg:w-2/3">
             <h6 className="text-xl">Aviso</h6>
-            <p>Coordinación de Universidad Abierta y Educación Digital de la UNAM. ©Todos los derechos reservados 2024. Hecho en México. Este sitio puede ser reproducido con fines no lucrativos, siempre y cuando no se mutile, se cite la fuente completa y su dirección electrónica, de otra forma, se requiere permiso previo por escrito de la Institución.</p>
-            <p>Al navegar en este sitio, encontrará contenidos diseñados por académicos de la UNAM, denominados Recursos Educativos Abiertos (REA), disponibles para todo el público en forma gratuita. Los contenidos de cada REA son responsabilidad exclusiva de sus autores y no tienen impedimento en materia de propiedad intelectual; asimismo, no contienen información que por su naturaleza pueda considerarse confidencial y reservada.</p>
-            <p>Los REA podrán ser utilizarlos sin fines de lucro, citando invariablemente la fuente y sin alterar la obra, respetando los términos institucionales de uso y los derechos de propiedad intelectual de terceros.</p>
+            <p>
+              Coordinación de Universidad Abierta y Educación Digital de la
+              UNAM. ©Todos los derechos reservados 2024. Hecho en México. Este
+              sitio puede ser reproducido con fines no lucrativos, siempre y
+              cuando no se mutile, se cite la fuente completa y su dirección
+              electrónica, de otra forma, se requiere permiso previo por escrito
+              de la Institución.
+            </p>
+            <p>
+              Al navegar en este sitio, encontrará contenidos diseñados por
+              académicos de la UNAM, denominados Recursos Educativos Abiertos
+              (REA), disponibles para todo el público en forma gratuita. Los
+              contenidos de cada REA son responsabilidad exclusiva de sus
+              autores y no tienen impedimento en materia de propiedad
+              intelectual; asimismo, no contienen información que por su
+              naturaleza pueda considerarse confidencial y reservada.
+            </p>
+            <p>
+              Los REA podrán ser utilizarlos sin fines de lucro, citando
+              invariablemente la fuente y sin alterar la obra, respetando los
+              términos institucionales de uso y los derechos de propiedad
+              intelectual de terceros.
+            </p>
           </div>
           <div className="lg:w-1/6"></div>
         </div>
