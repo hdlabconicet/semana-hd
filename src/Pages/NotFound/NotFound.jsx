7f8dc2b5aff017e533605e12047cbad8d299a8bf
Omar Terrazas <terrazas.omar@gmail.com>
2024-04-12 19:07:15 -0600

    Formato de página Not Found

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,8 +1,21 @@
 function NotFound() {
   return (
-    <>
-      <h1>Not Found</h1>
-    </>
+    <section>
+      <div className="text-center">
+        <h1 className="text-2xl font-medium leading-relaxed mb-12">
+          Esta ruta no conduce a información de<br />la Semana de Humanidades
+          Digitales
+        </h1>
+        <p>
+          <a
+            href="/"
+            className="bg-red_hd inline-block text-gray-50 p-2 rounded"
+          >
+            regresar al inicio
+          </a>
+        </p>
+      </div>
+    </section>
   );
 }
 
