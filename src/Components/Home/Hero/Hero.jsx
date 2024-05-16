5f2ac8411a33cc58192c11e937cb3342937bee1e
Omar Terrazas <terrazas.omar@gmail.com>
2024-05-16 13:06:40 -0600

    Ajuste de texto en componente Hero

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -14,8 +14,7 @@ const Hero = () => {
         </div>
         <div className="mt-10 lg:mt-0 lg:w-1/2 p-4 2xl:pl-10 text-center">
           <p className="mb-12 md:text-lg 2xl:text-2xl leading-relaxed 2xl:leading-10">
-          La SemanaHD es un evento abierto, gratuito y colaborativo que tiene como objetivo dar visibilidad a los proyectos e iniciativas sobre Humanidades Digitales en América Latina y el Caribe. Si tienes un evento relacionado con las HD que ocurra entre el 20 y 25 de mayo, te invitamos a compartirlo y visibilizarlo en esta plataforma.</p>
-          <p>La <a href="https://twitter.com/search?q=%23SemanaHD&src=typed_query" target="_blank" rel="noopener noreferrer">#SemanaHD</a> se propone como un espacio en línea para dar a conocer proyectos de Humanidades digitales a través de charlas, paneles, mesas de debates y talleres, etcétera. ¡Te invitamos a compartir tu evento en el calendario de actividades!</p>
+          La <a href="https://twitter.com/search?q=%23SemanaHD&src=typed_query" target="_blank" rel="noopener noreferrer">#SemanaHD</a> es un evento abierto, gratuito y colaborativo  que se propone como un espacio en línea para dar a conocer proyectos  e iniciativas de Humanidades digitales en América Latina y el Caribe.  Si tienes un evento relacionado con las humanidades digitales (charlas, paneles, mesas de debates y talleres, etcétera) que ocurra entre el 20 y 25 de mayo, te invitamos a compartirlo y visibilizarlo en esta plataforma.</p>
           <ColorBar />
           <h4 className="mt-12 text-green_hd font-semibold text-xl md:text-2xl xl:text-3xl">
             del 20 al 25 de mayo de 2024
