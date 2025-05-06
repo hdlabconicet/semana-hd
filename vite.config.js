0188fd1d185f3653698dcb0a0632e95cfff39ac3
JoshuaOB <joshuaortizbaco@gmail.com>
2025-05-06 13:27:08 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'semana-hd',
+  base: '/semana-hd/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
+  publicDir: 'public',
 })
-
