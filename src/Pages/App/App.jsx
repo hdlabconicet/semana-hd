0ad84f0062867e0c69e7118fa84cd8d5de2f0bcd
Julián Spinelli <julian.spinelli7@gmail.com>
2025-08-09 12:42:29 -0300

    remuevo footer

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -88,46 +88,7 @@ function App() {
       </nav>
       <AppRoutes />
       <footer>
-        <div className="container mx-auto p-2 text-white text-sm flex flex-col lg:flex-row">
-          <div className="lg:w-1/6 mb-6">
-            <p className="mt-7 mb-4">Desarrollado por:</p>
-            <p>
-              <a
-                href="https://bunam.unam.mx/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-              </a>
-            </p>
-          </div>
-          <div className="lg:w-2/3">
-            <h6 className="text-xl">Aviso</h6>
-            <p>
-              Coordinación de Universidad Abierta y Educación Digital de la
-              UNAM. ©Todos los derechos reservados 2024. Hecho en México. Este
-              sitio puede ser reproducido con fines no lucrativos, siempre y
-              cuando no se mutile, se cite la fuente completa y su dirección
-              electrónica, de otra forma, se requiere permiso previo por escrito
-              de la Institución.
-            </p>
-            <p>
-              Al navegar en este sitio, encontrará contenidos diseñados por
-              académicos de la UNAM, denominados Recursos Educativos Abiertos
-              (REA), disponibles para todo el público en forma gratuita. Los
-              contenidos de cada REA son responsabilidad exclusiva de sus
-              autores y no tienen impedimento en materia de propiedad
-              intelectual; asimismo, no contienen información que por su
-              naturaleza pueda considerarse confidencial y reservada.
-            </p>
-            <p>
-              Los REA podrán ser utilizarlos sin fines de lucro, citando
-              invariablemente la fuente y sin alterar la obra, respetando los
-              términos institucionales de uso y los derechos de propiedad
-              intelectual de terceros.
-            </p>
-          </div>
-          <div className="lg:w-1/6"></div>
-        </div>
+       
       </footer>
     </BrowserRouter>
   );
